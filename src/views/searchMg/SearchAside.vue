7b92b28adab10e302e16d5fac3a9661caec953bf
Rcsdy <1781056469@qq.com>
2023-02-12 20:50:32 +0800

    查询管理组件的实现

---
+++ src/views/searchMg/SearchAside.vue
@@ -0,0 +1,144 @@
+<template>
+    <ul>
+        <el-tree
+            :data="data"
+            :props="defaultProps"
+            accordion
+            @node-click="handleNodeClick">
+        </el-tree>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name:'SearchAside',
+        props:['sectorClick'],
+        data() {
+            return {
+                data: [
+                {
+                    id: 100,
+                    label: '项目部',
+                },{
+                    id: 101,
+                    label: '重庆总站',
+                    children: [{
+                        id: 101101,
+                        label: '重庆总站下属部门',
+                }]
+                }, {
+                    id: 102,
+                    label: '川南总站',
+                    children: [{
+                        id: 102102,
+                        label: '川南总站下属部门'
+                    }, {
+                        id: 6,
+                        label: '川南总站下属部门'
+                    }]
+                }, {
+                    id: 103,
+                    label: '川西总站',
+                    children: [{
+                        id: 7,
+                        label: '川西总站下属部门'
+                    }, {
+                        id: 8,
+                        label: '川西总站下属部门'
+                    }]
+                },{
+                    id: 104,
+                    label: '成都总站',
+                    children: [{
+                        id: 7,
+                        label: '成都总站下属部门'
+                    }, {
+                        id: 8,
+                        label: '成都总站下属部门'
+                    }]
+                },{
+                    id: 105,
+                    label: '办公室',
+                },{
+                    id: 106,
+                    label: '财务共享中心',
+                },{
+                    id: 107,
+                    label: '物联网业务部',
+                },{
+                    id: 108,
+                    label: '党群科',
+                },{
+                    id: 109,
+                    label: '规划计划部',
+                },{
+                    id: 110,
+                    label: '财务科',
+                },{
+                    id: 111,
+                    label: '纪委办公室',
+                },{
+                    id: 112,
+                    label: '人事科',
+                },{
+                    id: 113,
+                    label: '生产运行科',
+                },{
+                    id: 114,
+                    label: '企业法规科',
+                },{
+                    id: 115,
+                    label: '质量安全环保科',
+                },{
+                    id: 116,
+                    label: '物资采购管理部',
+                },{
+                    id: 117,
+                    label: '勘探与生产数据中心',
+                },{
+                    id: 118,
+                    label: '网络安全监督中心',
+                },{
+                    id: 119,
+                    label: '建设项目管理部',
+                },{
+                    id: 120,
+                    label: '信息系统业务部',
+                },{
+                    id: 121,
+                    label: '信息技术应用研究所',
+                }],
+                defaultProps: {
+                    children: 'children',
+                    label: 'label'
+                }
+            };
+        },
+        methods: {
+            handleNodeClick(data) {
+                this.sectorClick(data)
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    ul{
+        /* width: 100%;
+        height: 100%; */
+        background-color: #E9EEF3;  
+        list-style: none;  
+        margin: 0px; 
+        padding: 20px;
+    }
+
+    li{
+        background-color: #E9EEF3;
+        padding-top: 6px;
+    }
+
+    li:hover{
+        background-color: #f7fafd;
+        border-radius: 6px
+    }
+</style>
--- src/views/searchMg/index.vue
+++ src/views/searchMg/index.vue
@@ -1,15 +1,370 @@
 <template>
     <div class="app-container">
-        查询管理组件
+        <el-container>
+            <el-header>
+                <SearchHeader :searchData="searchData" :updateTableData="updateTableData"></SearchHeader>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <SearchAside :sectorClick="sectorClick"></SearchAside>
+                </el-aside>
+                <el-main>
+                    <SearchMain :tableData="tableData"></SearchMain>
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'SearchManagement',
-}
+    import SearchMain from './SearchMain.vue';
+    import SearchHeader from './SearchHeader.vue';
+    import SearchAside from './SearchAside.vue';
+    export default {
+        name: 'SearchManagement',
+        components:{SearchMain,SearchHeader,SearchAside},
+        data() {
+            return {
+                tableData: [{
+                    date: '2022-09-04',
+                    name: 55,
+                    level: '级别2',
+                    category: '党建',
+                    sector: '项目部',
+                    dutyPerson: '老幺',
+                    timeout:0,
+                    status:1
+                },{
+                    date: "2001-03-24",
+                    name: 84,
+                    level: "级别2",
+                    category: "glcgb",
+                    sector: "重庆总站",
+                    dutyPerson: "熊杰",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "2001-01-09",
+                    name: 91,
+                    level: "级别2",
+                    category: "iele",
+                    sector: "项目部",
+                    dutyPerson: "秦超",
+                    timeout: 0,
+                    status: 1
+                }, {
+                    date: "2016-02-15",
+                    name: 9,
+                    level: "级别2",
+                    category: "xcfq",
+                    sector: "党群科",
+                    dutyPerson: "叶刚",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "2018-08-31",
+                    name: 81,
+                    level: "级别2",
+                    category: "dxuim",
+                    sector: "重庆总站",
+                    dutyPerson: "陆芳",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "1970-09-18",
+                    name: 99,
+                    level: "级别2",
+                    category: "tfu",
+                    sector: "党群科",
+                    dutyPerson: "姜秀兰",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "1991-06-04",
+                    name: 114,
+                    level: "级别2",
+                    category: "gkg",
+                    sector: "项目部",
+                    dutyPerson: "苏杰",
+                    timeout: 0,
+                    status: 0
+                },{
+                    date: "2010-02-20",
+                    name: 27,
+                    level: "级别2",
+                    category: "eehh",
+                    sector: "重庆总站",
+                    dutyPerson: "秦娜",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "2006-01-24",
+                    name: 53,
+                    level: "级别2",
+                    category: "eeb",
+                    sector: "党群科",
+                    dutyPerson: "夏军",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "2014-03-23",
+                    name: 96,
+                    level: "级别2",
+                    category: "jguf",
+                    sector: "重庆总站",
+                    dutyPerson: "吕勇",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "1989-04-27",
+                    name: 58,
+                    level: "级别2",
+                    category: "ddhlr",
+                    sector: "规划计划部",
+                    dutyPerson: "黄涛",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "1981-12-18",
+                    name: 58,
+                    level: "级别2",
+                    category: "myht",
+                    sector: "党群科",
+                    dutyPerson: "李静",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "2018-04-18",
+                    name: 114,
+                    level: "级别2",
+                    category: "gcmo",
+                    sector: "规划计划部",
+                    dutyPerson: "康娟",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "1997-06-19",
+                    name: 88,
+                    level: "级别2",
+                    category: "srxj",
+                    sector: "党群科",
+                    dutyPerson: "潘涛",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "1972-11-01",
+                    name: 49,
+                    level: "级别2",
+                    category: "liw",
+                    sector: "项目部",
+                    dutyPerson: "吴伟",
+                    timeout: 0,
+                    status: 0
+                },{
+                    date: "1983-10-01",
+                    name: 5,
+                    level: "级别2",
+                    category: "ikfk",
+                    sector: "规划计划部",
+                    dutyPerson: "袁涛",
+                    timeout: 1,
+                    status: 1
+                },{
+                    date: "1972-05-10",
+                    name: 103,
+                    level: "级别2",
+                    category: "fxg",
+                    sector: "企业法规科",
+                    dutyPerson: "孟勇",
+                    timeout: 0,
+                    status: 0
+                },{
+                    date: "1987-03-27",
+                    name: 7,
+                    level: "级别2",
+                    category: "wykgx",
+                    sector: "项目部",
+                    dutyPerson: "范敏",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "2000-10-07",
+                    name: 39,
+                    level: "级别2",
+                    category: "kbvhn",
+                    sector: "企业法规科",
+                    dutyPerson: "高艳",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "1981-10-31",
+                    name: 76,
+                    level: "级别2",
+                    category: "xpfmw",
+                    sector: "项目部",
+                    dutyPerson: "黎敏",
+                    timeout: 0,
+                    status: 0
+                },{
+                    date: "1972-03-20",
+                    name: 62,
+                    level: "级别2",
+                    category: "unpx",
+                    sector: "成都总站",
+                    dutyPerson: "梁静",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "2013-11-08",
+                    name: 116,
+                    level: "级别2",
+                    category: "sbz",
+                    sector: "企业法规科",
+                    dutyPerson: "陈刚",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "2011-03-25",
+                    name: 74,
+                    level: "级别2",
+                    category: "fel",
+                    sector: "项目部",
+                    dutyPerson: "白超",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "1989-01-14",
+                    name: 6,
+                    level: "级别2",
+                    category: "elpm",
+                    sector: "企业法规科",
+                    dutyPerson: "夏娜",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "2005-04-12",
+                    name: 8,
+                    level: "级别2",
+                    category: "wjqr",
+                    sector: "成都总站",
+                    dutyPerson: "史娟",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "2018-05-10",
+                    name: 92,
+                    level: "级别2",
+                    category: "dndd",
+                    sector: "项目部",
+                    dutyPerson: "邹刚",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "1985-01-11",
+                    name: 3,
+                    level: "级别2",
+                    category: "gfwny",
+                    sector: "企业法规科",
+                    dutyPerson: "薛强",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "1974-11-28",
+                    name: 112,
+                    level: "级别2",
+                    category: "hgdb",
+                    sector: "成都总站",
+                    dutyPerson: "黄秀英",
+                    timeout: 0,
+                    status: 0
+                },{
+                    date: "1971-02-15",
+                    name: 63,
+                    level: "级别2",
+                    category: "xsfb",
+                    sector: "项目部",
+                    dutyPerson: "谢娜",
+                    timeout: 1,
+                    status: 0
+                },{
+                    date: "1986-03-11",
+                    name: 94,
+                    level: "级别2",
+                    category: "vzqx",
+                    sector: "企业法规科",
+                    dutyPerson: "胡娜",
+                    timeout: 0,
+                    status: 1
+                },{
+                    date: "1970-08-14",
+                    name: 51,
+                    level: "级别2",
+                    category: "dhod",
+                    sector: "成都总站",
+                    dutyPerson: "陈霞",
+                    timeout: 1,
+                    status: 1
+                }],
+            }
+        },
+        methods:{
+            searchData(nameInput,dateInput){
+                if (nameInput) {//搜索名称
+                    this.tableData = this.tableData.filter((data) => {
+                        return data.name === parseInt(nameInput)
+                    })
+                }
+                if (dateInput) {//搜索时间
+                    this.tableData = this.tableData.filter((data) => {
+                        return data.date === dateInput
+                    })
+                }
+                console.log(this.tableData)
+            },
+            sectorClick(newdata){
+                this.tableData = this.tableData.filter((data) => {
+                    return data.sector === newdata.label
+                })
+            },
+            updateTableData(){
+                //return 请求数据
+            }
+        }
+    }
 </script>
 
-<style>
-
+<style scoped>
+    .el-header{
+        background-color: #f9fafc;
+        color: #333;
+        /* text-align: center;
+        line-height: 60px; */
+    }
+    
+    .el-aside {
+        background-color: #E9EEF3;
+        color: #333;
+        /* text-align: center;
+        line-height: 200px; */
+    }
+    
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        /* text-align: center;
+        line-height: 160px; */
+    }
+    
+    body > .el-container {
+        margin-bottom: 40px;
+    }
+    
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
+    }
+    
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
+    }
 </style>
